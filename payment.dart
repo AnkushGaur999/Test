b511eea1df43249d0b2cc9d2d8ca39c7a4857fd8
Ankush Gaur <ankugaur999@gmail.com>
2025-06-19 12:59:54 +0530

    Add Payment

---
+++ payment.dart
@@ -0,0 +1,13 @@
+
+class PaymentProvider{
+  // This class will handle payment processing
+  void processPayment(double amount) {
+    // Logic to process payment
+    print("Processing payment of \$${amount}");
+  }
+
+  void refundPayment(double amount) {
+    // Logic to refund payment
+    print("Refunding payment of \$${amount}");
+  }
+}
